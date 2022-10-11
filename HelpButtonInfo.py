a3e46b2b055f3ae2b34ec9ace34b9cb6733f4a24
GiveMePseudonyms <nathanhart@ntlworld.com>
2022-10-11 17:04:32 +0100

    Added help buttons for settings.

--- HelpButtonInfo.py
+++ HelpButtonInfo.py
@@ -61,4 +61,91 @@ tooltips = {
     '''
     Selects the colour dictionary to be used.
     ''',
+
+    'pen size':
+    '''
+    Determines the thickness of each line.
+    ''',
+        
+    'wait until done':
+    '''
+    Updates the canvas only after the process has finished. This speeds up processing times.
+    ''',
+
+    'frame skip interval':
+    '''
+    Determines how many frames will pass between canvas updates. This can speed up processing times whilst allowing the user to see the drawing process.
+    ''',
+
+    'move distance':
+    '''
+    Determines how far the turtle will move at each step.
+    ''',
+
+    'output filename':
+    '''
+    Names the output file for the screenshot. Don't add the extension to the name. Files are saved as PNGs.
+    ''',
+
+    'digit is colour':
+    '''
+    Each pen stroke/pixel will correspond to the digit it represents.
+    ''',
+
+    'digit colour palette':
+    '''
+    Determines which colour palette will be used for the 'Digit is colour' setting.
+    ''',
+
+    'cycle colours':
+    '''
+    Changes the colours in a cyclical manner each frame.
+    ''',
+
+    'cycle palette':
+    '''
+    Determines which colour palette will be used in the 'Cycle colours' setting.
+    ''',
+
+    'cycle change rate':
+    '''
+    Determines how fast the colour pallette will cycle.
+    ''',
+
+    'static colours':
+    '''
+    Keeps the colours static, overrides any other colour settings.
+    ''',
+
+    'digit is pen size':
+    '''
+    Changes the pen size to correspond to the digit being represented.
+    ''',
+
+    'pen size scaling factor':
+    '''
+    Determines the scaling factor for the pen size when using the \'Digit is pen size\' setting.
+    ''',
+
+    'colour scheme': 
+    '''
+    Determines the colour scheme to be used for the sandpile grains.
+    ''',
+
+    'colour palette':
+    '''
+    Determines which colour palette will be used.
+    ''',
+
+    'random offset bound':
+    '''
+    In the web visualiser, each line can be offset by a given amount. The upper and lower bound settings determine the upper and lower bounds for this random amount.
+    ''',
+
+    'line opacity':
+    '''
+    Sets the opacity of the line. Lower numbers are more transparent.
+    ''',
+
+
 }
--- SettingsData.py
+++ SettingsData.py
@@ -39,6 +39,12 @@ class SettingsData:
 
         self.all_hlp_buttons = [
             self.pixel_visualiser_options.help_buttons,
+            self.turtle_visualiser_options.help_buttons,
+            self.spiral_visualiser_options.help_buttons,
+            self.waveform_visualiser_options.help_buttons,
+            self.sandpile_visualiser_options.help_buttons,
+            self.web_visualiser_options.help_buttons,
+            self.orbital_visualiser_options.help_buttons,
         ]
 
         self.selection = ''
@@ -210,52 +216,68 @@ class SettingsData:
             self.lbl_combobox_pen_size = ttk.Label(WINDOW, text='Pen size')
             self.combobox_pen_size = ttk.Combobox(WINDOW, state='readonly')
             self.combobox_pen_size['values'] = ['3', '6', '9', '12', '15']
+            self.hlp_combobox_pen_size = tk.Button(WINDOW, text='?', command=lambda: mbox_help(WINDOW, 'pen size'))
 
             self.chk_wait_until_done = ttk.Checkbutton(WINDOW, text='Wait until done')
+            self.hlp_wait_until_done = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'wait until done'))
 
             self.lbl_combobox_frame_skip_interval = ttk.Label(WINDOW, text='Frame skip interval')
             self.combobox_frame_skip_interval = ttk.Combobox(WINDOW, state='readonly')
             self.combobox_frame_skip_interval['values'] = ['5000', '10000', '15000', '20000']
+            self.hlp_combobox_frame_skip_interval = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'frame skip interval'))
 
             self.lbl_textfield_target = ttk.Label(WINDOW, text='Target')
             self.textfield_target = ttk.Entry(WINDOW)
+            self.hlp_textfield_target = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'target'))
 
             self.lbl_combobox_move_distance = ttk.Label(WINDOW, text='Move distance')
             self.combobox_move_distance = ttk.Combobox(WINDOW, state='readonly')
             self.combobox_move_distance['values'] = ['0.2', '0.4', '0.6', '0.8', '1']
+            self.hlp_combobox_move_distance = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'move distance'))
             
             self.lbl_textfield_output_filename = ttk.Label(WINDOW, text='Screenshot filename')
             self.textfield_output_filename = ttk.Entry(WINDOW)
+            self.hlp_textfield_output_filename = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'output filename'))
 
             self.chk_digit_is_colour = ttk.Checkbutton(WINDOW, text='Digit corresponds to colour')
+            self.hlp_chk_digit_is_colour = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'digit is colour'))
 
             self.lbl_combobox_digit_colour_palette = ttk.Label(WINDOW, text='Digit colour palette')
             self.combobox_digit_colour_palette = ttk.Combobox(WINDOW, state='readonly')
             self.combobox_digit_colour_palette['values'] = ['Starfield', 'Yellow and Purple', 'Mint']
+            self.hlp_combobox_digit_colour_palette = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'digit colour palette'))
 
             self.chk_cycle_colours = ttk.Checkbutton(WINDOW, text='Cycle colours')
+            self.hlp_chk_cycle_colours = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'cycle colours'))
 
             self.lbl_combobox_cycle_palette = ttk.Label(WINDOW, text='Cycle Palette')
             self.combobox_cycle_palette = ttk.Combobox(WINDOW, state='readonly')
             self.combobox_cycle_palette['values'] = ['Cherry Blossom', 'All Black', 'Metallic Flake', 'Blue Autumn']
+            self.hlp_combobox_cycle_palette = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'cycle palette'))
 
             self.lbl_combobox_cycle_change_rate = ttk.Label(WINDOW, text='Cycle change rate')
             self.combobox_cycle_change_rate = ttk.Combobox(WINDOW, state='readonly')
             self.combobox_cycle_change_rate['values'] = ['0.001', '0.003', '0.005', '0.007']
+            self.hlp_combobox_cycle_change_rate = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'cycle change rate'))
 
             self.chk_static_colours = ttk.Checkbutton(WINDOW, text='Static Colours')
+            self.hlp_chk_static_colours = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'static colours'))
 
             self.chk_add_randomness_to_colour = ttk.Checkbutton(WINDOW, text='Add randomness to colour')
+            self.hlp_chk_add_randomness_to_colour = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'add randomness to colour'))
 
             self.lbl_combobox_bg_colour = ttk.Label(WINDOW, text='Background Colour')
             self.combobox_bg_colour = ttk.Combobox(WINDOW, state='readonly')
             self.combobox_bg_colour['values'] = ['White', 'Black', 'Space Black', 'Red', 'Green', 'Blue']
+            self.hlp_combobox_bg_colour = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'background colour'))
 
             self.chk_digit_is_pen_size = ttk.Checkbutton(WINDOW, text='Digit is pen size')
+            self.hlp_chk_digit_is_pen_size = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'digit is pen size'))
             
             self.lbl_combobox_digit_is_pen_size_scale_factor = ttk.Label(WINDOW, text='Pen size scaling factor')
             self.combobox_digit_is_pen_size_scale_factor = ttk.Combobox(WINDOW, state='readonly')
             self.combobox_digit_is_pen_size_scale_factor['values'] = ['0.5', '1', '1.5']
+            self.hlp_combobox_digit_is_pen_size_scale_factor = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'pen size scaling factor'))
 
             self.widgets = []
             self.widgets.extend([
@@ -297,7 +319,22 @@ class SettingsData:
             ]
 
             self.help_buttons = [
-                None
+                self.hlp_wait_until_done,
+                self.hlp_combobox_pen_size,
+                self.hlp_combobox_frame_skip_interval,
+                self.hlp_textfield_target,
+                self.hlp_combobox_move_distance,
+                self.hlp_textfield_output_filename,
+                self.hlp_chk_digit_is_colour,
+                self.hlp_combobox_digit_colour_palette,
+                self.hlp_chk_cycle_colours,
+                self.hlp_combobox_cycle_palette,
+                self.hlp_combobox_cycle_change_rate,
+                self.hlp_chk_static_colours,
+                self.hlp_chk_add_randomness_to_colour,
+                self.hlp_combobox_bg_colour,
+                self.hlp_chk_digit_is_pen_size,
+                self.hlp_combobox_digit_is_pen_size_scale_factor,
             ]
     
     class SpiralVisualiserOptions:
@@ -311,14 +348,17 @@ class SettingsData:
             self.lbl_combobox_bg_colour = ttk.Label(WINDOW, text='Background colour')
             self.combobox_bg_colour = ttk.Combobox(WINDOW, state='readonly')
             self.combobox_bg_colour['values'] = ['Space Black', 'White', 'Black', 'Red', 'Green', 'Blue']
+            self.hlp_combobox_bg_colour = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'background colour'))
 
             self.lbl_combobox_offset_increment = ttk.Label(WINDOW, text='Offset increment')
             self.combobox_offset_increment = ttk.Combobox(WINDOW, state='readonly')
             self.combobox_offset_increment['values'] = ['0.25', '0.5', '0.75', '1']
+            self.hlp_combobox_offset_increment = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'offset increment'))
 
             self.lbl_combobox_digit_colour_palette = ttk.Label(WINDOW, text='Digit colour palette')
             self.combobox_digit_colour_palette = ttk.Combobox(WINDOW, state='readonly')
             self.combobox_digit_colour_palette['values'] = ['Starfield', 'Yellow and Purple', 'Mint', 'Peach and Purple']
+            self.hlp_combobox_digit_colour_palette = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'digit colour palette'))
 
             self.widgets= [
                 self.combobox_bg_colour,
@@ -333,7 +373,9 @@ class SettingsData:
             ]
 
             self.help_buttons = [
-                None
+                self.hlp_combobox_bg_colour,
+                self.hlp_combobox_offset_increment,
+                self.hlp_combobox_digit_colour_palette,
             ]
 
     class WaveformVisualiserOptions:
@@ -346,10 +388,12 @@ class SettingsData:
             self.lbl_combobox_bg_colour = ttk.Label(WINDOW, text='Background colour')
             self.combobox_bg_colour = ttk.Combobox(WINDOW, state='readonly')
             self.combobox_bg_colour['values'] = ['Space Black', 'White', 'Black', 'Red', 'Green', 'Blue']
+            self.hlp_combobox_bg_colour = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'background colour'))
 
             self.lbl_combobox_digit_colour_palette = ttk.Label(WINDOW, text='Digit colour palette')
             self.combobox_digit_colour_palette = ttk.Combobox(WINDOW, state='readonly')
             self.combobox_digit_colour_palette['values'] = ['Starfield', 'Yellow and Purple', 'Mint', 'Peach and Purple']
+            self.hlp_combobox_digit_colour_palette = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'digit colour palette'))
 
             self.widgets = [
                 self.combobox_bg_colour,
@@ -362,7 +406,8 @@ class SettingsData:
             ]
 
             self.help_buttons = [
-                None
+                self.hlp_combobox_bg_colour,
+                self.hlp_combobox_digit_colour_palette,
             ]
 
     class SandPileVisualiserOptions:
@@ -376,14 +421,17 @@ class SettingsData:
             self.lbl_combobox_bg_colour = ttk.Label(WINDOW, text='Background colour')
             self.combobox_bg_colour = ttk.Combobox(WINDOW, state='readonly')
             self.combobox_bg_colour['values'] = ['Space Black', 'White', 'Black', 'Red', 'Green', 'Blue']
+            self.hlp_combobox_bg_colour = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'background colour'))
 
             self.lbl_combobox_colour_scheme = ttk.Label(WINDOW, text='Colour scheme')
             self.combobox_colour_scheme = ttk.Combobox(WINDOW, state='readonly')
             self.combobox_colour_scheme['values'] = ['Sand', 'Pastels', 'Square', 'Washed Pastels']
+            self.hlp_combobox_colour_scheme = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'colour scheme'))
 
             self.lbl_combobox_update_interval = ttk.Label(WINDOW, text='Update interval')
             self.combobox_update_interval = ttk.Combobox(WINDOW, state='readonly')
             self.combobox_update_interval['values'] = ['1', '10', '20', '50', '100', '250', '1000']
+            self.hlp_combobox_update_interval = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'frame skip interval'))
 
             self.widgets = [
                 self.combobox_bg_colour,
@@ -398,7 +446,9 @@ class SettingsData:
             ]
 
             self.help_buttons = [
-                None
+                self.hlp_combobox_bg_colour, 
+                self.hlp_combobox_colour_scheme,
+                self.hlp_combobox_update_interval,
             ]
 
     class WebVisualiserOptions:
@@ -414,21 +464,26 @@ class SettingsData:
 
             self.lbl_textfield_target = ttk.Label(WINDOW, text='Target')
             self.textield_target = ttk.Entry(WINDOW)
+            self.hlp_textfield_target = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'target'))
 
             self.lbl_textfield_update_interval = ttk.Label(WINDOW, text='Update interval')
             self.textfield_update_interval = ttk.Entry(WINDOW)
+            self.hlp_textfield_update_interval = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'frame skip interval'))
 
             self.lbl_combobox_random_offset_upper_bound = ttk.Label(WINDOW, text='Random offset upper bound')
             self.combobox_random_offset_upper_bound = ttk.Combobox(WINDOW, state='readonly')
             self.combobox_random_offset_upper_bound['values'] = ['10', '20', '30', '40', '50']
+            self.hlp_combobox_random_offset_upper_bound = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'random offset bound'))
 
             self.lbl_combobox_random_offset_lower_bound = ttk.Label(WINDOW, text='Random offset lower bound')
             self.combobox_random_offset_lower_bound = ttk.Combobox(WINDOW, state='readonly')
             self.combobox_random_offset_lower_bound['values'] = ['0', '1', '2', '3', '4', '5']
+            self.hlp_combobox_random_offset_lower_bound = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'random offset bound'))
 
             self.lbl_combobox_colour_palette = ttk.Label(WINDOW, text='Colour palette')
             self.combobox_colour_palette = ttk.Combobox(WINDOW, state='readonly')
             self.combobox_colour_palette['values'] = ['Starfield', 'Yellow and Purple', 'Mint', 'Peach and Purple']
+            self.hlp_combobox_colour_palette = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'colour palette'))
 
             self.chk_use_random_colours = ttk.Checkbutton(WINDOW, text='Use random colours')
 
@@ -451,7 +506,11 @@ class SettingsData:
             ]
 
             self.help_buttons = [
-                None
+                self.hlp_textfield_target,
+                self.hlp_textfield_update_interval,
+                self.hlp_combobox_random_offset_upper_bound,
+                self.hlp_combobox_random_offset_lower_bound,
+                self.hlp_combobox_colour_palette,
             ]
 
     class OrbitalVisualiserOptions:
@@ -463,6 +522,7 @@ class SettingsData:
             self.lbl_combobox_line_opacity = ttk.Label(WINDOW, text='Line opacity')
             self.combobox_line_opacity = ttk.Combobox(WINDOW, state='readonly')
             self.combobox_line_opacity['values'] = ['0', '10', '20', '30', '40', '50', '60', '70', '80', '90', '100']
+            self.hlp_combobox_line_opacity = tk.Button(WINDOW, text='?', command= lambda: mbox_help(WINDOW, 'line opacity'))
 
             self.widgets = [
                 self.combobox_line_opacity,
@@ -473,5 +533,5 @@ class SettingsData:
             ]
 
             self.help_buttons = [
-                None
+                self.hlp_combobox_line_opacity,
             ]
